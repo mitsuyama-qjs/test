e141d1abff0b091665df9525820bc73b414ffb03
mitsuyama-qjs <mitsuyamavich@gmail.com>
2018-11-09 18:08:26 +0900

    test for if not pull before push

---
+++ empty.sh
@@ -0,0 +1,36 @@
+crnt0=`pwd`
+
+cass=(ccc)
+ions=(ttt)
+num=100
+
+for cas in ${cass[@]}
+do
+for ion in ${ions[@]}
+do
+
+
+for i in `seq -f "%02g" 5 9` 
+do 
+for j in `seq -f "%02g" 0 12`
+do 
+
+crnt1=`pwd`
+
+	echo p${num}_${i}_${j} 
+	cd   p${num}_${i}_${j} 
+
+	
+	cd ../  
+
+cd $crnt1
+	
+done
+done
+
+
+cd $crnt0	
+
+
+done
+done
